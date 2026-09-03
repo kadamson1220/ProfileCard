32da046814d8a2940e347bac5a685858397228de
kadamson1220 <kayliadamson@gmail.com>
2026-09-02 22:45:54 -0400

    final edits for hw submission

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,27 +5,34 @@ export default function HomeScreen() {
   return (
   <View style={styles.container}>
     <View style={styles.card}>
-      <Image style={styles.profileImage} source={{ uri: "https://picsum.photos/200"}} />
+      <Image style={styles.profileImage} source={require('../../assets/images/profile2.jpeg')}/>
       <Text style={styles.textEdit}>Kayli Adamson</Text>
-      <Text style={styles.bioEdit}>lorem ipsum lala doa erjakjeeamae florem sowerds</Text>
-      <View style={styles.factsEdit}>
-        <Text style={styles.fact}>• I go to school</Text>
-        <Text style={styles.fact}>• I go to work</Text>
-        <Text style={styles.fact}>• I go run</Text>
-      </View>
+      <Text style={styles.bioEdit}>Student at IUI pursuing a dual degree in Computer Science and Digital Forensics, and a certification in Full-Stack.</Text>
+
+      
     </View>
+    <View style={styles.factsEdit}>
+        <Text style={styles.fact}>• I am a server, website manager, and DFU intern</Text>
+        <Text style={styles.fact}>• I enjoy running and cycling</Text>
+        <Text style={styles.fact}>• I like gold jewelry</Text>
+      </View>
   </View>
   );
 }
 
 const styles = StyleSheet.create({
   factsEdit: {
-    width: "100%",
+    width: 300,
     gap: 8,
+    backgroundColor: "lavender",
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 15,
+    alignItems: "center",
   },
   fact: {
     fontSize: 16,
-    //textAlign: "center",
+    textAlign: "center",
   },
   card: {
     width: 300,
@@ -33,19 +40,22 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 15,
     alignItems: "center",
+    borderWidth: 7,
+    borderColor: "lavender",
+    borderStyle: "dotted",
   },
-  profileImage:{
-    width: 120, 
-    height: 120,
-    borderRadius: 60,
-    marginBottom: 15,
-  },
+ profileImage: {
+  width: 170,
+  height: 170,
+  borderRadius: 85,
+  marginBottom: 15,
+},
   container: {
     backgroundColor: "lightblue",
     flex: 1, 
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 10,
+    borderWidth: 15,
     borderColor: "white",
     borderRadius: 20,
     borderStyle: "dotted",
